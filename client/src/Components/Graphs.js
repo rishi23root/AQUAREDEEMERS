48e8788b64653d25ad82d74730582f823c41d894
surajcool203 <chaudharysuraj0404@gamil.com>
2022-08-22 20:34:41 +0530

    updated folder structure

--- client/src/Components/Graphs.js
+++ client/src/Components/Graphs.js
@@ -1,4 +1,4 @@
-import { Box, Typography } from '@mui/material';
+import { Box} from '@mui/material';
 import React from 'react'
 
 import { Line } from 'react-chartjs-2';
@@ -9,26 +9,26 @@ const Graphs = () => {
 
   return (
    
-    <div>
+    <div> 
         <Box>
-         <Line
-            datasetIdKey='id'
-            data={{
-              labels: ['Jun', 'Jul', 'Aug'],
-              datasets: [
-                {
-                  id: 1,
-                  label: '',
-                  data: [5, 6, 7],
-                },
-                {
-                  id: 2,
-                  label: '',
-                  data: [3, 2, 1],
-                },
-              ],
-            }}
-          />
+        <Line
+        datasetIdKey='id'
+        data={{
+          labels: ['Jun', 'Jul', 'Aug'],
+          datasets: [
+            {
+              id: 1,
+              label: '',
+              data: [5, 6, 7],
+            },
+            {
+              id: 2,
+              label: '',
+              data: [3, 2, 1],
+            },
+          ],
+        }}
+      />
          </Box>
     </div>
   );
